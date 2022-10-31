623937f124e8555531e9b3fd9aab4a186e60ea44
0xjoshva <joshuasteed123@gmail.com>
2022-10-31 20:17:12 +0200

    readme

--- README.md
+++ README.md
@@ -1,19 +1,99 @@
-# nft-card
+# Frontend Mentor - NFT preview card component solution
 
-## Project setup
-```
-npm install
+This is a solution to the [NFT preview card component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/nft-preview-card-component-SbdUL_w0U). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Frontend Mentor - NFT preview card component solution](#frontend-mentor---nft-preview-card-component-solution)
+  - [Table of contents](#table-of-contents)
+  - [Overview](#overview)
+    - [The challenge](#the-challenge)
+    - [Screenshot](#screenshot)
+    - [Links](#links)
+  - [My process](#my-process)
+    - [Built with](#built-with)
+    - [What I learned](#what-i-learned)
+    - [Continued development](#continued-development)
+  - [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size
+- See hover states for interactive elements
+
+### Screenshot
+
+![](./src/assets/preview.png)
+
+### Links
+
+- Solution URL: [https://www.frontendmentor.io/challenges/nft-preview-card-component-SbdUL_w0U/hub/nft-card-done-with-vuejs-IDUM2v_ko5](https://www.frontendmentor.io/challenges/nft-preview-card-component-SbdUL_w0U/hub/nft-card-done-with-vuejs-IDUM2v_ko5)
+- Live Site URL: [https://illustrious-donut-a7cabf.netlify.app/](https://illustrious-donut-a7cabf.netlify.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- [Vue.js](https://vuejs.org/) - Vue Framework
+  
+### What I learned
+
+- learnt how to overlay a div on an image
+  
+```html
+<div class="wrapper" @mouseover="monkey = true" @mouseleave="monkey = !true">
+<img src="../assets/image-equilibrium.jpg" alt="" class="image">
+<div class="overlay" v-if="monkey">
+<img src="../assets/icon-view.svg" alt="" >
+</div>
+</div>
 ```
 
-### Compiles and hot-reloads for development
-```
-npm run serve
+```css
+.wrapper{
+  position: relative;
+  width: 320px;
+  height: 320px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.image{
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+}
+.overlay{
+  position: relative;
+  z-index: 10;
+  background: hsla(178, 100%, 50%, 0.397);
+  width: 320px;
+  height: 320px ;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 7px;
+}
+.overlay img{
+  height: 50px;
+  width: auto;
+}
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
+### Continued development
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+- needs a bit of responsiveness
+
+
+## Author
+
+- Website - [Joshua Steed](https://www.joshuasteed.com)
+- Frontend Mentor - [@0xjoshva](https://www.frontendmentor.io/profile/0xjoshva)
--- src/views/CardView.vue
+++ src/views/CardView.vue
@@ -1,37 +1,49 @@
 <template>
- <section>
-  <div class="card">
-<div class="wrapper" @mouseover="monkey = true" @mouseleave="monkey = !true">
-<img src="../assets/image-equilibrium.jpg" alt="" class="image">
-<div class="overlay" v-if="monkey">
-<img src="../assets/icon-view.svg" alt="" >
-</div>
-</div>
-<h3 class="title">Equilibrium #3429</h3>
-<p class="description">Our Equilibrium collection promotes balance and calm.</p>
-<div class="badges">
-  <div class="price"> <img src="../assets/icon-ethereum.svg" alt=""> <p>0.041 ETH</p></div>
-  <div class="days"> <img src="../assets/icon-clock.svg" alt=""> <p>3 days left</p> </div>
-</div>
-<!-- <hr> -->
-<div class="created">
-  <img src="../assets/image-avatar.png" alt="" class="avatar">
-<p>Creation of <span>Jules Wyvern</span></p>
-</div>
-  </div>
- </section>
+  <section>
+    <div class="card">
+      <div
+        class="wrapper"
+        @mouseover="monkey = true"
+        @mouseleave="monkey = !true"
+      >
+        <img src="../assets/image-equilibrium.jpg" alt="" class="image" />
+        <div class="overlay" v-if="monkey">
+          <img src="../assets/icon-view.svg" alt="" />
+        </div>
+      </div>
+      <h3 class="title">Equilibrium #3429</h3>
+      <p class="description">
+        Our Equilibrium collection promotes balance and calm.
+      </p>
+      <div class="badges">
+        <div class="price">
+          <img src="../assets/icon-ethereum.svg" alt="" />
+          <p>0.041 ETH</p>
+        </div>
+        <div class="days">
+          <img src="../assets/icon-clock.svg" alt="" />
+          <p>3 days left</p>
+        </div>
+      </div>
+      <!-- <hr> -->
+      <div class="created">
+        <img src="../assets/image-avatar.png" alt="" class="avatar" />
+        <p>Creation of <span>Jules Wyvern</span></p>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
 export default {
   data() {
     return {
-      monkey: false
-    }
-  }
-}
+      monkey: false,
+    };
+  },
+};
 </script>
 <style scoped>
-section{
+section {
   width: 100%;
   height: 100vh;
   background: var(--main-bg);
@@ -40,7 +52,7 @@ section{
   align-items: center;
 }
 
-.card{
+.card {
   width: 23rem;
   height: fit-content;
   padding: 1.5rem;
@@ -50,96 +62,95 @@ section{
   border-radius: 17px;
   box-shadow: 0px 20px 20px 20px #0000002b;
 }
-.image{
+.image {
   border-radius: 10px;
   position: relative;
 }
 
-.title{
+.title {
   color: var(--white);
-  font-family: 'Outfit', sans-serif;
-  padding-bottom: .5rem;
+  font-family: "Outfit", sans-serif;
+  padding-bottom: 0.5rem;
   padding-top: 1rem;
   font-weight: 600;
 }
-.description{
-color: var(--softblue);
-font-family: 'Outfit', sans-serif;
-padding-bottom: 1rem;
-font-weight: 300;
+.description {
+  color: var(--softblue);
+  font-family: "Outfit", sans-serif;
+  padding-bottom: 1rem;
+  font-weight: 300;
 }
-.badges{
+.badges {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  font-family: 'Outfit', sans-serif;
+  font-family: "Outfit", sans-serif;
   padding-bottom: 1.5rem;
   /* line */
   border-bottom: 1px solid var(--line);
   margin-bottom: 1rem;
 }
-.price{
+.price {
   display: flex;
-  align-items: center; 
-  column-gap: .5rem;
+  align-items: center;
+  column-gap: 0.5rem;
   color: var(--cyan);
   font-weight: 600;
 }
-.days{
+.days {
   display: flex;
   align-items: center;
-   column-gap: .5rem;
-   color: var(--softblue);
-}
-hr{
- height: 1px;
- background: var(--softblue);
-}
-.created{
-  display: flex;
-  font-family: 'Outfit', sans-serif;
-  column-gap: .5rem;
-}
-.created p{
+  column-gap: 0.5rem;
   color: var(--softblue);
 }
-.created span{
+hr {
+  height: 1px;
+  background: var(--softblue);
+}
+.created {
+  display: flex;
+  font-family: "Outfit", sans-serif;
+  column-gap: 0.5rem;
+}
+.created p {
+  color: var(--softblue);
+}
+.created span {
   color: var(--white);
 }
-.avatar{
+.avatar {
   height: 30px;
   border: 1px solid white;
   border-radius: 50%;
-  width:auto;
+  width: auto;
 }
-.wrapper{
- position: relative;
- width: 320px;
- height: 320px;
- display: flex;
- align-items: center;
- justify-content: center;
+.wrapper {
+  position: relative;
+  width: 320px;
+  height: 320px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
-.image{
+.image {
   position: absolute;
   top: 0;
   left: 0;
   z-index: 1;
 }
-.overlay{
+.overlay {
   position: relative;
-z-index: 10;
-background: hsla(178, 100%, 50%, 0.397);
-width: 320px;
-height: 320px ;
-display: flex;
-align-items: center;
-justify-content: center;
+  z-index: 10;
+  background: hsla(178, 100%, 50%, 0.397);
+  width: 320px;
+  height: 320px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   border-radius: 7px;
 }
-.overlay img{
+.overlay img {
   height: 50px;
   width: auto;
-
 }
 </style>
